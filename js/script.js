65dc069d3841e2cede4f8aa833a4bab6df62533c
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-04-23 16:18:48 +0200

    add button

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
 // Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 
 const emailListElemnt = document.getElementById(`email-list`);
+const createEmailButton = document.getElementById(`button-create-mail`);
 
 const emailNumber = 10;
 
@@ -22,14 +23,31 @@ function generateLiElement(element) {
 
 const emailArray = [];
 
-for (let i = 0; i < emailNumber; i++) {
-  axios
-    .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-    .then((response) => {
-      const randomEmail = response.data.response; // accedo ai dati (data) della risposta(response) con key response(response)
-      console.log(randomEmail);
+// for (let i = 0; i < emailNumber; i++) {
+//   axios
+//     .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+//     .then((response) => {
+//       const randomEmail = response.data.response; // accedo ai dati (data) della risposta(response) con key response(response)
+//       console.log(randomEmail);
 
-      const liElement = generateLiElement(randomEmail);
-      emailListElemnt.innerHTML += liElement;
-    });
-}
+//       const liElement = generateLiElement(randomEmail);
+//       emailListElemnt.innerHTML += liElement;
+//     });
+// }
+
+createEmailButton.addEventListener("click", () => {
+  // elimino i figli dell'elemento ad ogni click del bottone
+  emailListElemnt.replaceChildren();
+  // genero una nuova lista di email
+  for (let i = 0; i < emailNumber; i++) {
+    axios
+      .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+      .then((response) => {
+        const randomEmail = response.data.response; // accedo ai dati (data) della risposta(response) con key response(response)
+        console.log(randomEmail);
+
+        const liElement = generateLiElement(randomEmail);
+        emailListElemnt.innerHTML += liElement;
+      });
+  }
+});
